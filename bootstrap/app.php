b6b0efd76c2d9f53c5e86314451203521fdd0355
steeven-js <jacques.steeven@gmail.com>
2024-12-17 12:00:23 -0400

    Mise à jour de la validation des tokens CSRF pour exclure les routes API spécifiques

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,9 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->validateCsrfTokens(except: [
+            'api/*',
             'webhook',
             'stripe/*',
-            'api/*',
+            'api/stripe/*'
         ]);
 
         //
