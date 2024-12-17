1c66a6e0f2907a921a79ad65fa9af833021f4de9
steeven-js <jacques.steeven@gmail.com>
2024-12-17 09:35:47 -0400

    Ajout d'une exception pour les tokens CSRF dans les routes API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'webhook',
             'stripe/*',
+            'api/*',
         ]);
 
         //
